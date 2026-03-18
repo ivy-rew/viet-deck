dedb5dc02d5593e2c74e867a4618a71e70a6eb91
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-03-18 20:24:50 +0100

    better viet; no noise

--- extract.sh
+++ extract.sh
@@ -34,7 +34,7 @@ for SLIDE in "$SLIDES_DIR"/*.mp4; do
   convert "$IMG_DIR/${BASENAME}.jpg" -threshold 80% "$SLIDES_DIR/scene_bw/${BASENAME}.jpg"
   # Use the BW image for OCR
   BW_IMG="$SLIDES_DIR/scene_bw/${BASENAME}.jpg"
-  tesseract -l vie+eng --psm 6 "$BW_IMG" "$SLIDES_DIR/scene_text/${BASENAME}" > /dev/null 2>&1
+  tesseract -l vie+eng --psm 4 "$BW_IMG" "$SLIDES_DIR/scene_text/${BASENAME}" > /dev/null 2>&1
 
   # Use OpenCV script to crop main picture from each scene image
   SCENE_PICTURE_DIR="$SLIDES_DIR/scene_picture"
